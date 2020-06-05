551aac6aa1f02973a0a41e645e70305da9529b3d
Marine <marine.jacob.pro@gmail.com>
2020-06-05 15:35:01 +0200

    les commentaires c'est la vie

--- ar2_test1.py
+++ ar2_test1.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python3
+
+#  Starting the simulation
 import pybullet
+
+# pour pouvoir utiliser les délias (time.sleep)
 import time
+
 import pybullet_data
+
+# pour pouvoir utiliser la comemande système git clone
 import os
 
+
 pybullet.connect(pybullet.GUI)
+
+# pour remettre à zéro la simulation
 pybullet.resetSimulation()
+
 pybullet.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+# plane est le sol il est contenu dans l'import pybullet_data
 pybuplane = pybullet.loadURDF("plane.urdf")
+
 os.system("git clone https://github.com/Chris-Annin/ROS_AR2_urdf.git ar2")
+
+# on récpère le bras robot depuiq le dossier git qu'on a récupéré
+# c'est important de lui mettre une useFixedBase à 1 pour qu'il ne tombe pas (avec la gravité par exemple)
 robot = pybullet.loadURDF("ar2/urdf/ar2.urdf", [0, 0, 0], useFixedBase=1)
+
 pybullet.resetDebugVisualizerCamera(1.40, -53.0, -39.0, (0.53, 0.21, -0.24))
 time.sleep(2)
 
